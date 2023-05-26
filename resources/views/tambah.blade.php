6cd277f1532b24faca5a144f8cd06c07ecbce968
akshal <akshaldzikril.majid@neuron.co.id>
2023-05-26 23:37:41 +0700

    projectna

---
+++ resources/views/tambah.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TAMBAH CRUD</title>
+</head>
+<body>
+ 
+	
+	<h3>Data Pegawai</h3>
+ 
+	<a href="/pegawai"> Kembali</a>
+	
+	<br/>
+	<br/>
+ 
+	<form action="/pegawai/store" method="post">
+		{{ csrf_field() }}
+		Nama <input type="text" name="nama"> <br/>
+		Jabatan <input type="text" name="jabatan"> <br/>
+		Umur <input type="number" name="umur"> <br/>
+		Alamat <textarea name="alamat"></textarea> <br/>
+		<input type="submit" value="Simpan Data">
+	</form>
+		
+ 
+ 
+</body>
+</html>
